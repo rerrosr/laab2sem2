c564609b6d570df16fde082e999ae3024a890a69
Feona <108477623+rerrosr@users.noreply.github.com>
2023-04-02 23:46:55 +0300

    Update funct2.h

--- laab2sem2/funct2.h
+++ laab2sem2/funct2.h
@@ -23,7 +23,7 @@ typedef struct SinglyLinkedList{
 void singlyListInfo(SinglyLinkedList*);
 int checkIndex(SinglyLinkedList*,const char*);
 void addInfo(FILE*, SinglyLinkedList *);
-void printCompressedFile(FILE*,FILE*, char**, char**,int);
+void printCompressFile(FILE*,FILE*, char**, char**,int);
 int mostPopularWord(SinglyLinkedList*, int);
 int mostUnpopularWord(SinglyLinkedList*, int);
 void printSestertius(FILE*, char**, char**,int);
@@ -33,25 +33,5 @@ void eraseNode(SinglyLinkedList*,const char *);
 int profit(SinglyLinkedList*,int,int);
 
 #endif LAAB2SEM2_FUNCT2_H
-//typedef struct NodeWord {
-//    int count;
-//    char word[100];
-//    struct NodeWord *next;
-//    struct NodeWord *prev;
-//}  NodeWord;
-//Структура для представления узла связного списка
-//typedef struct Node {
-//    int size;
-//    struct NodeWord *head;
-//    struct NodeWord *tail;
-//} Node ;
-//void add_word(struct Node *node, const char *buffer);
-//void print(Node *node);
-//int is_word_character(char c);
-//void add_word(Node **queue, char *word);
-//void print_queue(Node *queue);
-//Node *find_most_popular_long_word(Node *queue, int min_length);
-//Node *find_least_popular_short_word(Node *queue, int max_length);
-//void compress(Node *queue, int min_length, int max_length, char *input_filename, char *output_filename);
-//void decompress(Node *queue, int min_length, int max_length, char *input_filename, char *output_filename);
+
 
